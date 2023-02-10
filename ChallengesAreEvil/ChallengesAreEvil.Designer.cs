b13137012d4141beacf9cdc81be56f2ea6953133
CoUsT <gitea@coust.cloud>
2023-02-10 17:59:45 +0100

    First commit

---
+++ ChallengesAreEvil/ChallengesAreEvil.Designer.cs
@@ -0,0 +1,128 @@
+﻿﻿namespace ChallengesAreEvil
+{
+    partial class ChallengesAreEvilForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChallengesAreEvilForm));
+            this.removeTokensButton = new System.Windows.Forms.Button();
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.clearPrintButton = new System.Windows.Forms.Button();
+            this.nickTextBox = new System.Windows.Forms.TextBox();
+            this.printTextBox = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // removeTokensButton
+            // 
+            this.removeTokensButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.removeTokensButton.Location = new System.Drawing.Point(0, 0);
+            this.removeTokensButton.Margin = new System.Windows.Forms.Padding(0);
+            this.removeTokensButton.Name = "removeTokensButton";
+            this.removeTokensButton.Size = new System.Drawing.Size(632, 66);
+            this.removeTokensButton.TabIndex = 0;
+            this.removeTokensButton.Text = "Ban";
+            this.removeTokensButton.UseVisualStyleBackColor = true;
+            this.removeTokensButton.Click += new System.EventHandler(this.removeTokensButton_Click);
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.messageLabel.Location = new System.Drawing.Point(0, 92);
+            this.messageLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(632, 28);
+            this.messageLabel.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.clearPrintButton);
+            this.panel1.Controls.Add(this.nickTextBox);
+            this.panel1.Controls.Add(this.printTextBox);
+            this.panel1.Controls.Add(this.removeTokensButton);
+            this.panel1.Controls.Add(this.messageLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(632, 609);
+            this.panel1.TabIndex = 3;
+            // 
+            // clearPrintButton
+            // 
+            this.clearPrintButton.Location = new System.Drawing.Point(12, 568);
+            this.clearPrintButton.Name = "clearPrintButton";
+            this.clearPrintButton.Size = new System.Drawing.Size(608, 29);
+            this.clearPrintButton.TabIndex = 4;
+            this.clearPrintButton.Text = "Clear log";
+            this.clearPrintButton.UseVisualStyleBackColor = true;
+            this.clearPrintButton.Click += new System.EventHandler(this.clearPrintButton_Click);
+            // 
+            // nickTextBox
+            // 
+            this.nickTextBox.Location = new System.Drawing.Point(12, 69);
+            this.nickTextBox.Name = "nickTextBox";
+            this.nickTextBox.Size = new System.Drawing.Size(608, 20);
+            this.nickTextBox.TabIndex = 3;
+            // 
+            // printTextBox
+            // 
+            this.printTextBox.Location = new System.Drawing.Point(12, 123);
+            this.printTextBox.Multiline = true;
+            this.printTextBox.Name = "printTextBox";
+            this.printTextBox.Size = new System.Drawing.Size(608, 439);
+            this.printTextBox.TabIndex = 2;
+            // 
+            // ChallengesAreEvilForm
+            // 
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(632, 609);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(15, 15);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "ChallengesAreEvilForm";
+            this.Load += new System.EventHandler(this.ChallengesAreEvilForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button clearPrintButton;
+
+        private System.Windows.Forms.TextBox printTextBox;
+        private System.Windows.Forms.TextBox nickTextBox;
+
+        #endregion
+
+        private System.Windows.Forms.Label messageLabel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button removeTokensButton;
+    }
+}
---
+++ ChallengesAreEvil/ChallengesAreEvil.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChallengesAreEvil
+{
+    public partial class ChallengesAreEvilForm : Form
+    {
+
+        LeagueConnection lc;
+        
+        public ChallengesAreEvilForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ChallengesAreEvilForm_Load(object sender, EventArgs e)
+        {
+            lc = new LeagueConnection();
+        }
+
+        private void editMessageLabel(String msg)
+        {
+            messageLabel.Text = msg;
+        }
+
+        private Boolean checkIfLeagueIsConnected()
+        {
+            if (!lc.IsConnected)
+            {
+                editMessageLabel("Not connected to League Client! You need to log in first and wait few seconds. If you run LoL as admin, run this app as admin too!");
+                return false;
+            }
+            return true;
+        }
+
+        private async void removeTokensButton_Click(object sender, EventArgs e)
+        {
+            if (checkIfLeagueIsConnected())
+            {
+                String nick = nickTextBox.Text;
+                JsonArray summonerArray = (JsonArray) await lc.Post("/lol-summoner/v2/summoners/names", "[\"" + nick + "\"]");
+                if (summonerArray.Count == 0)
+                {
+                    appendText("Not found: " + nick);
+                    return;
+                }
+                JsonObject summoner = (JsonObject)summonerArray[0];
+                appendText(summoner.ToString());
+                
+                long summonerId = (long)summoner["summonerId"];
+                appendText("Summoner ID: " + summonerId);
+                
+                Random random = new Random();
+                
+                for (int i = 0; i < 10; i++)
+                {
+                    long gameId = random.Next();
+                    String body = "{\"gameId\":" + gameId + ",\"offenses\":\"NEGATIVE_ATTITUDE,VERBAL_ABUSE,LEAVING_AFK\",\"reportedSummonerId\":" + summonerId + "}";
+                    appendText(body);
+                    JsonObject reportResponse = (JsonObject) await lc.Post("/lol-end-of-game/v2/player-complaints", body);
+                    appendText(reportResponse.ToString());
+                }
+
+            }
+        }
+
+        private void appendText(String text)
+        {
+            printTextBox.AppendText(text + "\r\n");
+        }
+
+        private void clearPrintButton_Click(object sender, EventArgs e)
+        {
+            printTextBox.Clear();
+        }
+    }
+}
